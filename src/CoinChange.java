7d5586163691dbfcad93aacc1836d32f3cce97ce
seahorseZhang <33713384+seahorseZhang@users.noreply.github.com>
2019-08-13 22:31:27 +0800

    This commit will create the branch 'master'.

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -3,15 +3,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-//Solution to the question from : Bytedance
+//Solution to the coin change problem from : Bytedance
 //Z国的货币系统包含面值1元、4元、16元、64元共计4种硬币，以及面值1024元的纸币。现在小Y使用1024元的纸币购买了一件价值为的商品，请问最少他会收到多少硬币？
-public class Main1 {
+public class CoinChange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		int price = sc.nextInt();
+		int price = 1024 - sc.nextInt();
 		int res = 0;
 		List<Integer> coins = new LinkedList<Integer>();
 		coins.add(64);
@@ -19,7 +19,7 @@ public class Main1 {
 		coins.add(4);
 		coins.add(1);
 		Iterator<Integer> iter = coins.iterator();
-		while(iter.hasNext()){
+		while(price != 0){
 			int temp = iter.next();
 			res += price/temp;
 			price = price%temp;
